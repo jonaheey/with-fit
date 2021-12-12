23f97593bce8fcefe5d6ac9089f928b5cd343644
jonaheey <jonaheey@gmail.com>
2021-12-13 03:47:45 +0900

    1213 add webcam

--- fitness/views.py
+++ fitness/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+
+import cv2
+import threading
+
+from utils import load_pretrain_model
+from model.Pose.pose_visualizer import TfPoseVisualizer
+from model.Action.recognizer import load_action_premodel, framewise_recognize
+
+estimator = load_pretrain_model('VGG_origin')
+action_classifier = load_action_premodel('model/Action/stnading_knee_scene/framewise_standing_knee_scene2.5.h5')
 
 # 메인
 def index(request):
@@ -22,6 +35,7 @@ def select(request):
             exercise_img = 'standing_knee_up'
         else:
             exercise_img = 'step_forward_lunge'
+        print(exercise_img)
         
         # 인원 수 선택 화면으로 넘어감
         return render(
@@ -38,26 +52,78 @@ def select(request):
 
 # 플레이
 def play(request):
-    if request.method == 'POST':
-        # 선택된 운동
-        exercise_name = request.POST.get('exercise')
+    # 선택된 운동
+    exercise_name = request.POST.get('exercise')
 
-        # 선택된 인원수
-        player = request.POST.get('selected_p')
+    # 선택된 인원수
+    player = request.POST.get('selected_p')
 
-        # 팀 이름
-        team_name = request.POST.get('team_name')
+    # 팀 이름
+    team_name = request.POST.get('team_name')
 
-        print(exercise_name, player, team_name)
+    print(exercise_name, player, team_name)
 
-        # 플레이 화면
-        return render(
-            request,
-            'play.html',
-        )
+    # 플레이 화면
+    return render(
+        request,
+        'play.html',
+    )
 
-    else:
-        return redirect(
-            'fitness:index'
-        )
+
+
+# ! model 추가
+def home(request):
+    context = {}
+
+    return render(request, "play.html", context)
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+
+        self.player_list = []
+        # 임시 변수
+        self.player = 256
+        self.exercise = 0
+
+        for i in range(self.player):
+            self.player_list.append([i, self.exercise, 'now_pose', 0, False, False, False, False, False])
+
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        humans = estimator.inference(image)
+        pose = TfPoseVisualizer.draw_pose_rgb(image, humans)
+        image  = framewise_recognize(pose, action_classifier, self.player_list)
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read() 
+            # humans = estimator.inference(self.frame) 
+            # pose = TfPoseVisualizer.draw_pose_rgb(self.frame, humans)
+            # self.frame  = framewise_recognize(pose, self.player_list)
+
+cam = VideoCamera()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def webcam(request):
+    try:
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
     
+    except:  # This is bad! replace it with proper handling
+        print("에러입니다...")
+        pass
