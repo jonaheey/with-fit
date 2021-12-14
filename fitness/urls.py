36e69f4c2c4249035a742c4664b9bf221aa726da
Jonah <jonah@Jonah-MacBookAir.local>
2021-12-14 11:31:57 +0900

    1214 front end basic

--- fitness/urls.py
+++ fitness/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'), # 메인
     path('select/', views.select, name='select'), # 운동, 인원 수 선택
     path('play/', views.play, name='play'), # 플레이
-    path('webcam', views.webcam, name="webcam"), # 플레이 화면
+    # path('webcam', views.webcam, name="webcam"), # 플레이 화면
 ]
 
 # https://dev-yakuza.posstree.com/ko/django/view/
