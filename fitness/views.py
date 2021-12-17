c09c2d9047d15e97793d9aeb9d915ade24227390
22g1 <jd05081@naver.com>
2021-12-17 11:21:13 +0900

    1217 tmp

--- fitness/views.py
+++ fitness/views.py
@@ -8,33 +8,59 @@ def index(request):
     )
 
 # 운동, 인원수 선택
-def select(request):
-    # 운동 선택 후
-    if request.method == 'POST':
-        # 선택된 운동 값
+# def select(request):
+#     # 운동 선택 후
+#     if request.method == 'POST':
+#         # 선택된 운동 값
+#         exercise_name = request.POST.get('selected_e')
+
+#         if exercise_name == '푸시업':
+#             exercise_img = 'push_up'
+#         elif exercise_name == '사이드 런지':
+#             exercise_img = 'side_lunge'
+#         elif exercise_name == '스탠딩 니 업':
+#             exercise_img = 'standing_knee_up'
+#         else:
+#             exercise_img = 'step_forward_lunge'
+        
+#         # 인원 수 선택 화면으로 넘어감
+#         return render(
+#             request,
+#             'people.html',
+#             {'exercise_name' : exercise_name,
+#              'exercise_img' : exercise_img}
+#         )
+#     # 운동 선택 화면
+#     return render(
+#         request,
+#         'exercise.html'
+#     )
+
+def exercise(request):
+    return render(
+        request,
+        'exercise.html'
+    )
+
+def player(request):
+    if request.method == "POST":
         exercise_name = request.POST.get('selected_e')
 
-        if exercise_name == '푸시업':
+        if exercise_name == "푸시업":
             exercise_img = 'push_up'
         elif exercise_name == '사이드 런지':
-            exercise_img = 'side_lunge'
+            exercise_name = 'side_lunge'
         elif exercise_name == '스탠딩 니 업':
             exercise_img = 'standing_knee_up'
         else:
             exercise_img = 'step_forward_lunge'
-        
-        # 인원 수 선택 화면으로 넘어감
+
         return render(
             request,
             'people.html',
             {'exercise_name' : exercise_name,
              'exercise_img' : exercise_img}
         )
-    # 운동 선택 화면
-    return render(
-        request,
-        'exercise.html'
-    )
 
 # 플레이
 def play(request):
@@ -60,4 +86,9 @@ def play(request):
         return redirect(
             'fitness:index'
         )
-    
+
+def aja(request):
+    return render(
+        request,
+        'fetch.html'
+    )
